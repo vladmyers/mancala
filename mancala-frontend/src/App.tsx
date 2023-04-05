34cb348f6abee9242f7b2925a57e070fe302c63b
Vlad Myers <vladmyers.dev@gmail.com>
2023-04-05 17:40:28 +0300

    Refactor App.tsx, index.tsx and GameSession.tsx files

--- mancala-frontend/src/App.tsx
+++ mancala-frontend/src/App.tsx
@@ -7,14 +7,15 @@ import Registration from "./game/Registration";
 import WaitingRoom from "./game/WaitingRoom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GameSession from "./game/GameSession";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
-            {/*<Route path="/game-session" element={<GameSession />} />*/}
-            <Route path="/register" element={<Registration />} />
-            <Route path="/waiting-room" element={<WaitingRoom />} />
+            <Route path="/game-session" element={<GameSession/>}/>
+            <Route path="/register" element={<Registration/>}/>
+            <Route path="/waiting-room" element={<WaitingRoom/>}/>
             <Route path="/about" element={<About/>}/>
         </Routes>
     );
--- mancala-frontend/src/game/GameSession.tsx
+++ mancala-frontend/src/game/GameSession.tsx
@@ -1,64 +1,69 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ServiceLocator } from '../service/ServiceLocator';
+import {useNavigate} from 'react-router-dom';
+import {ServiceLocator} from '../service/ServiceLocator';
 import GameSessionService from '../service/GameSessionService';
 import Header from '../component/Header';
 import Body from '../component/Body';
-import { AxiosError, AxiosResponse } from 'axios';
-import { RestResponseError } from '../api/RestResponseError';
-import { RestResponse } from '../api/RestResponse';
+import {AxiosError, AxiosResponse} from 'axios';
+import {RestResponseError} from '../api/RestResponseError';
+import {RestResponse} from '../api/RestResponse';
 import {GameSessionDto} from "../dto/GameSessionDto";
 
 type GameSessionProps = {
-  waitingRoomUuid: string;
+    waitingRoomUuid?: string;
 };
 
-const GameSession = ({ waitingRoomUuid }: GameSessionProps) => {
-  useNavigate();
-  const gameSessionService: GameSessionService = ServiceLocator.get('gameSessionService')!;
+const GameSession = ({waitingRoomUuid}: GameSessionProps) => {
+    const navigate = useNavigate();
+    const gameSessionService: GameSessionService = ServiceLocator.get('gameSessionService')!;
 
-  const [errorMessage, setErrorMessage] = React.useState('');
-  const [gameSession, setGameSession] = React.useState<GameSessionDto>();
+    const [errorMessage, setErrorMessage] = React.useState('');
+    const [gameSession, setGameSession] = React.useState<GameSessionDto>();
 
-  React.useEffect(() => {
-    gameSessionService
-      .create(waitingRoomUuid)
-      .then((response: RestResponse<GameSessionDto>) => {
-        if (response.result) {
-          setGameSession(response.result);
+    React.useEffect(() => {
+        if (!waitingRoomUuid) {
+            navigate('/waiting-room');
+            return;
         }
-      })
-      .catch((e: AxiosError) => {
-        let axiosResponse = e?.response as AxiosResponse<RestResponseError>;
-        if (axiosResponse?.data) {
-          let restResponse = axiosResponse?.data as RestResponse<any>;
-          let restResponseError = restResponse.error!;
-          setErrorMessage(restResponseError.displayMessage + ': ' + restResponseError.message);
-        }
-      });
-  }, []);
 
-  return (
-    <>
-      <Header />
-      <Body>
-        <h1>Game Session</h1>
-        <div className="d-flex justify-content-left align-items-center" style={{ minHeight: '200px' }}>
-          {!errorMessage && gameSession && (
-            <div>
-              <div>Game session UUID: {gameSession.uuid}</div>
-              <div>Created date: {new Date(gameSession.createdDateTime).toLocaleString()}</div>
-            </div>
-          )}
-          {errorMessage && (
-            <div className="alert alert-danger" role="alert">
-              {errorMessage}
-            </div>
-          )}
-        </div>
-      </Body>
-    </>
-  );
+        gameSessionService
+            .create(waitingRoomUuid)
+            .then((response: RestResponse<GameSessionDto>) => {
+                if (response.result) {
+                    setGameSession(response.result);
+                }
+            })
+            .catch((e: AxiosError) => {
+                let axiosResponse = e?.response as AxiosResponse<RestResponseError>;
+                if (axiosResponse?.data) {
+                    let restResponse = axiosResponse?.data as RestResponse<any>;
+                    let restResponseError = restResponse.error!;
+                    setErrorMessage(restResponseError.displayMessage + ': ' + restResponseError.message);
+                }
+            });
+    }, []);
+
+    return (
+        <>
+            <Header/>
+            <Body>
+                <h1>Game Session</h1>
+                <div className="d-flex justify-content-left align-items-center" style={{minHeight: '200px'}}>
+                    {!errorMessage && gameSession && (
+                        <div>
+                            <div>Game session UUID: {gameSession.uuid}</div>
+                            <div>Created date: {new Date(gameSession.createdDateTime).toLocaleString()}</div>
+                        </div>
+                    )}
+                    {errorMessage && (
+                        <div className="alert alert-danger" role="alert">
+                            {errorMessage}
+                        </div>
+                    )}
+                </div>
+            </Body>
+        </>
+    );
 };
 
 export default GameSession;
