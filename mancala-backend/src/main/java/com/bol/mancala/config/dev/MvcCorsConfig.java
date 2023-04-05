5c29d1d7ffd2827859ff0db413b01fc95aa4fd29
Vlad Myers <vladmyers.dev@gmail.com>
2023-04-05 03:16:20 +0300

    MvcCorsConfig and local profile added for development

---
+++ mancala-backend/src/main/java/com/bol/mancala/config/dev/MvcCorsConfig.java
@@ -0,0 +1,21 @@
+package com.bol.mancala.config.dev;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Disable CORS for development
+ */
+@Configuration
+@Profile("local")
+public class MvcCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("*");
+    }
+
+}
